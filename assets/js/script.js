0907393e698eb9cf6151334f74b4494e49a226ec
Josh Carter <joshcarter8400@gmail.com>
2020-09-04 12:05:07 -0400

    added text input and time color features

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,35 @@ var timeDisplay = $("#currentDay");
 var theDay = moment();
 
 timeDisplay.text(theDay.format("dddd, hA"));
+
+
+
+var createItem = function() {
+    var itemE1 = $("textarea").addClass("description");
+    var itemEntry = S("<input>")
+        .addClass("description")
+        .text(itemE1)
+}
+
+var taskNotification = function() {
+    var currentTime = moment().format("H")
+
+    $("textarea").each(function() {
+        var hour = parseInt($(this).attr('data-hour'))
+        console.log(hour)
+        if (hour == currentTime) {
+            $(this).addClass("present")
+        } else if (hour > currentTime) {
+            $(this).addClass("future")
+        } else {
+            $(this).addClass("past")
+        }
+    })
+
+
+
+
+
+
+}
+taskNotification();
